7dd04a88e9c41b8b932b5a3310ace845255ee121
Dharadharu <51207024+Dharadharu@users.noreply.github.com>
2019-08-05 17:30:23 +0530

    Create poi.py

---
+++ poi.py
@@ -0,0 +1,7 @@
+ai=list(map(int,input().split()))
+bi=list(map(int,input().split()))
+ci=list(map(int,input().split()))
+if (ai[0]==bi[0]==ci[0]) or (ai[1]==bi[1]==ci[1]):
+    print("yes")
+else:
+    print("no")
