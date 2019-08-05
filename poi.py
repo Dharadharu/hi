cc0ec1f1433734c187994c48c3856de10f96a246
Dharadharu <51207024+Dharadharu@users.noreply.github.com>
2019-08-05 17:39:42 +0530

    Update poi.py

--- poi.py
+++ poi.py
@@ -1,7 +1,8 @@
 ai=list(map(int,input().split()))
 bi=list(map(int,input().split()))
 ci=list(map(int,input().split()))
-if (ai[0]==bi[0]==ci[0]) or (ai[1]==bi[1]==ci[1]):
+if (ai[0]==bi[0]==ci[0]) or (ai[1]==bi[1]==ci[1]) or (ai[0]==ai[1] and bi[0]==bi[1] and ci[0]==ci[1]):
     print("yes")
 else:
     print("no")
+
